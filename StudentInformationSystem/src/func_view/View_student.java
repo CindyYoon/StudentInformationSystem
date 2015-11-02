4e0b40bb25959134562a9b233c0e3221387bfb25
ykmulti0325 <dbsrud1541@naver.com>
2015-11-02 21:07:29 +0900

    Test :: make view func in main

--- StudentInformationSystem/src/func_view/View_student.java
+++ StudentInformationSystem/src/func_view/View_student.java
@@ -5,29 +5,30 @@ import java.util.Scanner;
 
 public class View_student {
 	
-	protected Statement stat = null;
-	protected Connection conn = null;
-	int id;  // 학번 입력
-	String name; // 이름 입력
-	String department; //학과 입력
-	int number;
+	protected static Statement stat = null;
+	protected static Connection conn = null;
+	static int id;  // 학번 입력
+	static String name; // 이름 입력
+	static String department; //학과 입력
+	static int phone_number;
 	
-	private void View_student() {
-		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost:3306/sw2015";
+//	private void View_student() {
+//		
+//		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+//			String url = "jdbc:mysql://localhost:3306/sw2015";
+//
+//			conn = DriverManager.getConnection(url, "root", "1234");
+//			stat = conn.createStatement();
+//			//initialize();
+//		} catch (Exception e) {
+//			e.printStackTrace(System.out);
+//		}
+//	}
+	
 
-			conn = DriverManager.getConnection(url, "root", "1234");
-			stat = conn.createStatement();
-			//initialize();
-		} catch (Exception e) {
-			e.printStackTrace(System.out);
-		}
-	}
-	
-	
-	public void View(int id, String name, String department, int number ) throws Exception {
+	public static void View() throws Exception {
+	//public void View(int id, String name, String department, int phone_number ) throws Exception {
 		
 		ResultSet rs = null;
 		Scanner input_some = new Scanner(System.in);
@@ -38,18 +39,17 @@ public class View_student {
 		if(id == 0){
 			System.out.println("존재하는 ID를 가진 학생이 없습니다.");
 		}else {
-			rs = stat.executeQuery("select* from StudentInformation when ID");
+			rs = stat.executeQuery("select* from StudentInformation when ID='"+id+"'");
 				
-			while(rs.next()){
-				id = rs.getInt(1);
-				name = rs.getString(2);
-				department = rs.getString(3);
-				number = rs.getInt(4);
+//			while(rs.next()){
+//				id = rs.getnextInt();
+//				name = rs.getString(2);
+//				department = rs.getString(3);
+//				phone_number = rs.getInt(4);
 
-			System.out.println(id + "\t" +name+ "\t" +department+  "\t" +number+ "\t" +"\n");
-		    }
+//			System.out.println(id + "\t" +name+ "\t" +department+  "\t" +phone_number+ "\t" +"\n");
+//		    }
 		}
-		
 	}
-
+		
 }
